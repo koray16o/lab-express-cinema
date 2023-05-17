571b51e79b6dada9cd480604e645507765d10e60
Koray Ojeda <korayatlanojeda@gmail.com>
2023-05-17 20:00:49 +0100

    done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,12 +3,14 @@ const Movie = require('../models/Movie.model');
 
 router.get('/movies', async (req, res) => {
   const moviesFromDB = await Movie.find();
-  res.render('.views/movies/movies', { movies: moviesFromDB });
+  console.log(moviesFromDB);
+  res.render('movies/movies', { movies: moviesFromDB });
 });
 
 router.get('/movie/:id', async (req, res) => {
   const movies = await Movie.findById(req.params.id);
-  res.render('.views/movies/movie-details', movies);
+  console.log(movies);
+  res.render('movies/movie-details', movies);
 });
 
 module.exports = router;
